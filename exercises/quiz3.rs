9e71c25f875103d5bf57733849585abc51cde48a
Zsc <zsc2333@outlook.com>
2025-02-02 23:54:43 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,17 +16,16 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-
-
-pub struct ReportCard {
-    pub grade: ,
+pub struct ReportCard<T>
+where
+    T: std::fmt::Display,
+{
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl<T> ReportCard<T> {
+impl<T: std::fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!(
             "{} ({}) - achieved a grade of {}",
